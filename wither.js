5aabf3f54602040e942e5da8657353d76590089d
Oljawave <96820757+Oljawave@users.noreply.github.com>
2022-04-19 15:17:01 +0600

    first commit

---
+++ wither.js
@@ -0,0 +1,20 @@
+const header = document.querySelector('header');
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY >= 450) {
+        header.classList.add("scrolled")
+    }
+    else if (window.scrollY <= 400) {
+        header.classList.remove("scrolled")
+    }
+});
+
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
